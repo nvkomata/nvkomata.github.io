38d84fea664af561cc6aaddebd17e8c9e3e55a8d
nu <143197164+nvkomata@users.noreply.github.com>
2023-12-26 16:45:21 +0100

    c

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nuleftus.github.io'
+    site: 'https://nvkomata.github.io'
 });
