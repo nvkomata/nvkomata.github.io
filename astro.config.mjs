edb254ebd896d9eb38b7ad400772fbc30fa17a49
nvkomata <linuko@proton.me>
2024-07-26 16:01:12 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nvkomata.github.io",
-  base: "/",
-  outDir: "public",
-  publicDir: "static",
   integrations: [icon()],
 });
