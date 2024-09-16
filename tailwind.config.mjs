edd1f91c687b0e58bd493e948ac0ad9ca16d4b61
nvkomata <linuko@proton.me>
2024-09-16 17:25:42 +0200

    rebrand, new parallax, new fonts, new 88x31, add bluesky, 404 page stuffs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,22 +2,28 @@
 const plugin = require("tailwindcss/plugin");
 
 export default {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {
-        extend: {
-            backgroundImage: {
-                eternalskies: "url('/backgrounds/eternalskies.jpg')",
-                tokyo: "url('/backgrounds/tokyo.jpg')",
-                field: "url('/backgrounds/field.jpg')",
-            },
-        },
-        fontFamily: {
-            'sans': ['Helvetica', 'ui-sans-serif', 'system-ui', 'sans-serif'],
-            'serif': ['ui-serif', 'serif'],
-            'mono': ['ui-monospace', 'SFMono-Regular', 'monospace'],
-            'monoer': ['"Iosevka Curly Slab"', 'ui-monospace'],
-            'lora': ['Lora', 'serif'],
-        }
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        eternalskies: "url('/backgrounds/eternalskies.avif')",
+        tokyo: "url('/backgrounds/tokyo.avif')",
+        field: "url('/backgrounds/field.avif')",
+        demons: "url('/backgrounds/demons.avif')",
+      },
     },
-    plugins: [],
+    fontFamily: {
+      sans: [
+        "Helvetica Neue",
+        "HelveticaNeue",
+        "Helvetica",
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+      ],
+      serif: ["Forum", "ui-serif", "serif"],
+      mono: ["ui-monospace", "SFMono-Regular", "monospace"],
+    },
+  },
+  plugins: [],
 };
